3845074785d17a83940a98afd9e7eabe13c933b3
Júnior Nunes <denisonprobable13@gmail.com>
2022-08-11 20:36:15 -0300

    feat: validating if cpf alread exists

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
 
 /**
  * cpf -  string
@@ -10,6 +15,23 @@ const app = express();
  */
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statment: [],
+  });
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
